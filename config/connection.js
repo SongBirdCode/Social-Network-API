3328fa4b089aa5f5989cb2071f5954fc5610d1b0
SongBirdCode <Songbirdcode@gmail.com>
2024-05-23 17:32:23 -0400

    models completed

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+// Set up to require mongoose //
+
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/social-network",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+// Use this to log mongo queries being executed!
+mongoose.set("debug", true);
+
+module.exports = mongoose.connection;
